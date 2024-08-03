1e600dcbbe213e46e7703f1da6b8a1ee12f62320
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-03 17:38:58 +0300

    Sets the inputs to required

--- src/components/InputDetails.jsx
+++ src/components/InputDetails.jsx
@@ -5,23 +5,30 @@ const InputDetails = () => {
     <div>
       <div>
         <form action="GET" className="mt-20  flex flex-col items-center">
-          <h1 className="mb-5 text-5xl">Bank of Flatiron</h1>
+          <h1 className="mb-5 text-5xl text-white">Bank of Flatiron</h1>
           <input
             type="text"
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             placeholder="Payment Description"
+            required
           />
           <input
             type="text"
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             placeholder="Payment Category..."
+            required
           />
           <input
             type="number"
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             placeholder="Payment Amount..."
+            required
           />
-          <input type="date" className="bg-gray-500 mb-3 w-96 h-9 rounded " />
+          <input
+            type="date"
+            className="bg-gray-500 mb-3 w-96 h-9 rounded "
+            required
+          />
           <button className="bg-cyan-950 py-3 px-2 rounded">Submit</button>
         </form>
       </div>
