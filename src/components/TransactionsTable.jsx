3360ca0573fb050222f49457f75d631fc07675a5
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-04 00:42:08 +0300

    Fixes bug on the filter search.

--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const TransactionsTable = () => {
   const [transactions, setTransactions] = useState([]);
-  const [search, setSearch] = useState([]);
+  const [search, setSearch] = useState("");
   console.log(search);
   useEffect(() => {
     fetch("http://localhost:3000/transactions")
@@ -12,15 +12,18 @@ const TransactionsTable = () => {
   }, []);
   return (
     <div>
+      <h2 className="flex flex-col items-center mt-4 text-3xl text-white">
+        Transactions
+      </h2>
       <form
         action=""
-        className="mt-20  flex flex-col items-center"
+        className="mt-12  flex flex-col items-center"
         onChange={(e) => setSearch(e.target.value)}
       >
         <input
           type="text"
           className="bg-gray-500 mb-3 w-96 h-9 rounded "
-          placeholder="search category..."
+          placeholder="Search transaction by description..."
           name="search"
           required
         />
@@ -40,7 +43,7 @@ const TransactionsTable = () => {
               .filter((td) => {
                 return search.toLowerCase() === ""
                   ? td
-                  : td.category.toLowerCase().includes(search);
+                  : td.description.toLowerCase().includes(search);
               })
               .map((td, index) => (
                 <tr key={index}>
