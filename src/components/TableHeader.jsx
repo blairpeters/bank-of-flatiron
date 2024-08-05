4c8aa6f7cc572db99fca23aefaa8e3f2fc5b272a
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-05 21:07:56 +0300

    shifts the transactions table headers to this component from the transactions table.

---
+++ src/components/TableHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TableHeader = () => {
+  return (
+    <>
+      <tr>
+        <th className="text-white">Description</th>
+        <th className="text-white">Category</th>
+        <th className="text-white">Amount</th>
+        <th className="text-white">Date</th>
+      </tr>
+    </>
+  );
+};
+
+export default TableHeader;
