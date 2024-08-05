bfee80785285fde965f007228df24f5a09c06023
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-05 21:08:41 +0300

    Removes the table data and table headers code and takes it to separate component then uses props to pass the data.

--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
+import TableHeader from "./TableHeader";
+import TableData from "./TableData";
 
 const TransactionsTable = () => {
   const [transactions, setTransactions] = useState([]);
   const [search, setSearch] = useState("");
   console.log(search);
+
   useEffect(() => {
     fetch("https://bank-of-flatiron-database.vercel.app/transactions")
       .then((response) => response.json())
-      .then((data) => setTransactions(data))
+      .then((td) => setTransactions(td))
       .catch((err) => console.log(err));
   }, []);
   return (
@@ -28,35 +31,26 @@ const TransactionsTable = () => {
           required
         />
       </form>
-      <div className="flex flex-row justify-center mt-3">
-        <table className="">
-          <thead className="">
-            <tr>
-              <th className="text-white">Description</th>
-              <th className="text-white">Category</th>
-              <th className="text-white">Amount</th>
-              <th className="text-white">Date</th>
-            </tr>
-          </thead>
-          <tbody className="bg-lime-300">
-            {transactions
-              .filter((td) => {
-                return search.toLowerCase() === ""
-                  ? td
-                  : td.description.toLowerCase().includes(search);
-              })
-              .map((td, index) => (
-                <tr key={index}>
-                  <td className="mr-3">{td.description}</td>
-
-                  <td className="bg-red-300">{td.category}</td>
-                  <td className="bg-green-300">{td.amount}</td>
-                  <td className="bg-yellow-300">{td.date}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
+      {
+        <div className="flex flex-row justify-center mt-3">
+          <table className="">
+            <thead className="">
+              <TableHeader />
+            </thead>
+            <tbody className="bg-lime-300">
+              {transactions
+                .filter((td) => {
+                  return search.toLowerCase() === ""
+                    ? td
+                    : td.description.toLowerCase().includes(search);
+                })
+                .map((td, index) => (
+                  <TableData key={index} td={td} />
+                ))}
+            </tbody>
+          </table>
+        </div>
+      }
     </div>
   );
 };
