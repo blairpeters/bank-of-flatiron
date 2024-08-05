fb9e117fbeca3fe3761374500eca147c466eeccc
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-05 23:32:35 +0300

    adds the handle change and handlesubmit function to allow set form data to be displayed on the table using POST method. sets the form to prevent default after an event. alls set the form to clear the input box onSubmit

--- src/components/InputDetails.jsx
+++ src/components/InputDetails.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-const InputDetails = () => {
+const InputDetails = ({ transactions, setTransactions }) => {
+  const [FormData, setFormData] = useState({
+    description: "",
+    category: "",
+    amount: "",
+    date: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setFormData({
+      ...FormData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(FormData),
+    })
+      .then((res) => res.json())
+      .then((transaction) => setTransactions([transaction, ...transactions]));
+    setFormData({
+      description: "",
+      category: "",
+      amount: "",
+      date: "",
+    });
+  };
   return (
     <div>
       <div>
-        <form action="" className="mt-12  flex flex-col items-center">
-          <h1 className="mb-5 text-5xl text-white ">Bank of Flatiron</h1>
+        <form
+          action=""
+          className="  flex flex-col items-center"
+          onSubmit={handleSubmit}
+        >
+          <h1 className="py-8 mb-5 text-5xl text-white ">Bank of Flatiron</h1>
           <h2 className="mt-4 mb-6 text-3xl text-white">Transaction Form</h2>
           <input
             type="text"
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             placeholder="Payment Description"
             name="description"
+            value={FormData.description}
+            onChange={handleChange}
             required
           />
           <input
@@ -19,6 +61,8 @@ const InputDetails = () => {
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             placeholder="Payment Category..."
             name="category"
+            value={FormData.category}
+            onChange={handleChange}
             required
           />
           <input
@@ -26,17 +70,22 @@ const InputDetails = () => {
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             placeholder="Payment Amount..."
             name="amount"
+            value={FormData.amount}
+            onChange={handleChange}
             required
           />
           <input
             type="date"
             className="bg-gray-500 mb-3 w-96 h-9 rounded "
             name="date"
+            value={FormData.date}
+            onChange={handleChange}
             required
           />
-          <button className="bg-cyan-950 hover:bg-white py-3 px-2 rounded">
-            Add Transaction
-          </button>
+          <input
+            type="submit"
+            className="bg-cyan-950 hover:bg-red-500 py-3 px-2 rounded"
+          />
         </form>
       </div>
     </div>
